29351b20bb524c6e4ad2a4025856120fb4e90bcf
yFayr-6a6y <kakanop7xiaomi@gmail.com>
2025-05-27 16:43:51 +0300

    new metod-objects

--- src/Book.java
+++ src/Book.java
@@ -24,6 +24,31 @@ public class Book {
     public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
     }
+
+    @Override
+    public String toString() {
+        return "\"" + title + "\" от " + author.toString() + ", опубликовано в " + publicationYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return publicationYear == book.publicationYear &&
+                title.equals(book.title) &&
+                author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + author.hashCode();
+        result = 31 * result + publicationYear;
+        return result;
+    }
 }
 
 class Author {
@@ -41,5 +66,28 @@ class Author {
 
     public String getLastName() {
         return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return firstName.equals(author.firstName) &&
+                lastName.equals(author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        return result;
     }
 }
